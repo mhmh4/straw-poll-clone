1fa2c890cc16a8e3821bdea7426e718bb013c4e0
u06 <u06_m@protonmail.com>
2021-08-19 13:01:59 -0400

    fix: disallow checking multiple checkboxes for single choice polls

--- straw_poll/static/index.js
+++ straw_poll/static/index.js
@@ -1,15 +1,6 @@
-// let options = document.getElementById('options');
-// console.log(options.children);
-// let foo = options;
-// console.log(foo)
-// // foo.addEventListener("click", () => {
-// //   console.log('hello');
-// //   options.innerHTML += (`<li><input type="text" name="" id="" placeholder="Enter poll option"></li>`);
-// // });
+const shareBtn = document.getElementById('share-btn');
 
-const shareBtn = document.getElementById("share-btn");
-
-shareBtn.addEventListener("click", () => {
+shareBtn.addEventListener('click', () => {
   navigator.clipboard.writeText(window.location.href);
-  alert("The url has been copied to your clipboard.")
+  alert('The URL has been copied to your clipboard.')
 });
