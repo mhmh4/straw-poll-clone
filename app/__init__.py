e9154a22a676c7897cee41483ece178cf8690bcb
u06 <u06_m@protonmail.com>
2021-08-19 22:30:52 -0400

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
-import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = "foo123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
