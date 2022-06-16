b769563f1a4a6d99450d79e132d869164ba1ddae
miah <86537561+u06@users.noreply.github.com>
2022-06-16 11:39:42 +0600

    Added more logic to functions.php

--- straw_poll/tmp/functions.php
+++ straw_poll/tmp/functions.php
@@ -1,41 +1,73 @@
 <?php
 
-function get_pdo()
+function get_pdo() : PDO
 {
-    $dsn = "";
+    $dsn = "mysql:dbname=straw_poll_clone;host=localhost";
     $username = "root";
     $password = "";
 
     try {
         $pdo = new PDO($dsn, $username, $password);
     } catch (PDOException $exception) {
-        echo "";
+        $error_message = $exception->getMessage();
+        echo $error_message;
+        exit(1);
     }
 
     return $pdo;
 }
 
-function poll_id_exists($poll_id)
+function doesPollExist($poll_id) : bool
 {
     $pdo = get_pdo();
-    $statement = $pdo->prepare("SELECT * FROM polls WHERE poll_id = ?");
-    $statement->execute(array($poll_id));
-}
-
-function create_poll($question, $options)
-{
-    $pdo = get_pdo();
-    $sql = "INSERT INTO polls (question, options, votes) VALUES (?, ?, ?)";
+    $sql = "SELECT * FROM polls WHERE poll_id = ?";
 
     $statement = $pdo->prepare($sql);
-    $statement->execute(array($question, $options));
+    $statement->execute(array($poll_id));
+
+    return $statement->rowCount() == 1;
 }
 
-function update_poll($poll_id, $option_number)
-{}
+function getPoll($poll_id)
+{
+    $pdo = get_pdo();
+    $sql = "SELECT * FROM polls WHERE poll_id = ?";
 
-function is_poll_expired()
-{}
+    $statement = $pdo->prepare($sql);
+    $statement->execute(array($poll_id));
 
-function delete_poll()
-{}
+    $row = $statement->fetch();
+    return $row;
+}
+
+function createPoll($question, $options, $allow_multiple_answers)
+{
+    // Filter out empty values and whitespace values from `options`.
+    $options = array_filter(
+        $options,
+        fn($value) => !is_null($value) && $value !== '' && !ctype_space($value)
+    );
+    $options_json = json_encode($options);
+
+    $num_options = count($options);
+
+    // If there are n options, then there must be n fields that stores
+    // the votes for those options.
+    $votes = array_map(fn() => 0, range(1, $num_options));
+    $votes_json = json_encode($votes);
+
+    $pdo = get_pdo();
+    $sql = 'INSERT INTO polls (question, options, votes, allow_multiple_answers) VALUES (?, ?, ?, ?)';
+
+    $statement = $pdo->prepare($sql);
+    $statement->execute(array($question, $options_json, $votes_json, $allow_multiple_answers));
+
+    // $row = $statement->fetch();
+    // return $row['poll_id'];
+}
+
+function updatePoll($poll_id, $option_number) {}
+
+function isPollExpired($poll) {}
+
+function deletePoll($poll) {}
