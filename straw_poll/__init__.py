fcdbd72b340ff7f037f8b48fdad6887cd5157663
miah <86537561+mhmh4@users.noreply.github.com>
2022-07-02 13:53:58 -0400

    Add Bootstrap to UI

--- straw_poll/__init__.py
+++ straw_poll/__init__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "foo123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 db = SQLAlchemy(app)
 
